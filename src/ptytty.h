bc17a892bfcfd678a95401a367d5a1c8bb7c2b66
sf-exg <sf-exg>
2011-05-16 20:58:27 +0000

    Remove dead code.

--- src/ptytty.h
+++ src/ptytty.h
@@ -42,15 +42,6 @@
 
 # include <pwd.h>
 
-# ifdef UTMP_SYSV
-#  ifndef USER_PROCESS
-#   define USER_PROCESS		7
-#  endif
-#  ifndef DEAD_PROCESS
-#   define DEAD_PROCESS		8
-#  endif
-# endif
-
 #endif
 
 #define fatal(msg) do { write (2, msg, sizeof (msg) - 1); _exit (255); } while (0)

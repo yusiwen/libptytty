699fa4a6317edfddf2fb5959182cdc8bbed19fbc
sf-exg <sf-exg>
2021-07-06 10:58:41 +0000

    Add cmake build system

---
+++ config.h.cmake
@@ -0,0 +1,44 @@
+#cmakedefine01 UTMP_SUPPORT
+#cmakedefine WTMP_SUPPORT
+#cmakedefine LASTLOG_SUPPORT
+
+#cmakedefine HAVE_PTY_H
+#cmakedefine HAVE_UTIL_H
+#cmakedefine HAVE_LIBUTIL_H
+#cmakedefine HAVE_SYS_IOCTL_H
+#cmakedefine HAVE_STROPTS_H
+
+#cmakedefine HAVE_REVOKE
+#cmakedefine HAVE__GETPTY
+#cmakedefine HAVE_GETPT
+#cmakedefine HAVE_POSIX_OPENPT
+#cmakedefine HAVE_ISASTREAM
+#cmakedefine01 HAVE_SETUID
+#cmakedefine01 HAVE_SETREUID
+#cmakedefine01 HAVE_SETRESUID
+
+#cmakedefine UNIX98_PTY
+#cmakedefine HAVE_OPENPTY
+
+#cmakedefine01 __EXTENSIONS__
+#cmakedefine _XOPEN_SOURCE ${_XOPEN_SOURCE}
+
+#cmakedefine HAVE_STRUCT_UTMP
+#cmakedefine HAVE_UTMP_HOST
+#cmakedefine HAVE_UTMP_PID
+#cmakedefine HAVE_UPDWTMP
+#cmakedefine HAVE_LASTLOG_H
+#cmakedefine HAVE_STRUCT_LASTLOG
+#cmakedefine PT_UTMP_FILE ${PT_UTMP_FILE}
+#cmakedefine PT_WTMP_FILE ${PT_WTMP_FILE}
+#cmakedefine PT_LASTLOG_FILE ${PT_LASTLOG_FILE}
+
+#cmakedefine HAVE_STRUCT_UTMPX
+#cmakedefine HAVE_UTMPX_HOST
+#cmakedefine HAVE_UPDWTMPX
+#cmakedefine HAVE_UPDLASTLOGX
+#cmakedefine HAVE_STRUCT_LASTLOGX
+#cmakedefine PT_WTMPX_FILE ${PT_WTMPX_FILE}
+#cmakedefine PT_LASTLOGX_FILE ${PT_LASTLOGX_FILE}
+
+#cmakedefine TTY_GID_SUPPORT
